6efc4c963cd1e42fc1bbcb9280412acdc846db3e
Deigo <diegonicolasriverasanchez@gmail.com>
2024-11-19 13:59:48 -0600

    Header, Footer and Guitar Components

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import {ref, reactive} from 'vue'
+import { db } from './data/guitarras'
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Guitar from './components/Guitar.vue';
+
+const guitarras = ref(db)
+
+
+</script>
+
+<template>
+    <Header />
+
+    <main class="container-xl mt-5">
+        <h2 class="text-center">Nuestra Colección</h2>
+
+        <div class="row mt-5"> 
+            <Guitar 
+                v-for="guitar in guitarras"
+                :guitar="guitar"
+                :key="guitar.id"
+            />
+        </div>
+    </main>
+
+    <Footer />
+</template>
+
+<style scoped>
+
+</style>
